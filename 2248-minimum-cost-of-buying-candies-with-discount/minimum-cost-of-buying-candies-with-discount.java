17e0f50db3c6ef310e0b08a8c1098424414dd3da
Abhinav Kumar Singh <150840703+abhi123108@users.noreply.github.com>
2026-06-01 21:39:18 +0530

    Time: 6 ms (41.78%) | Memory: 44.4 MB (83.72%) - LeetSync

---
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minimumCost(int[] cost) {
+
+        Arrays.sort(cost);
+
+        int ans = 0;
+        int cnt = 0;
+
+        for (int i = cost.length - 1; i >= 0; i--) {
+
+            cnt++;
+
+            if (cnt == 3) {
+                cnt = 0;      // free candy
+                continue;
+            }
+
+            ans += cost[i];
+        }
+
+        return ans;
+    }
+}
