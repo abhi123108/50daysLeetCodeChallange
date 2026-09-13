8edfa34edd8815536275e6585cfb635b45dbe7ab
Abhinav Kumar Singh <150840703+abhi123108@users.noreply.github.com>
2026-09-13 08:56:14 +0530

    Time: 27 ms (96.21%) | Memory: 45 MB (52.65%) - LeetSync

---
+++ 864-image-overlap/image-overlap.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int largestOverlap(int[][] img1, int[][] img2) {
+        int n = img1.length;
+        List<int[]> ones1 = new ArrayList<>();
+        List<int[]> ones2 = new ArrayList<>();
+
+        // Collect coordinates of all '1' bits
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < n; c++) {
+                if (img1[r][c] == 1) {
+                    ones1.add(new int[]{r, c});
+                }
+                if (img2[r][c] == 1) {
+                    ones2.add(new int[]{r, c});
+                }
+            }
+        }
+
+        if (ones1.isEmpty() || ones2.isEmpty()) {
+            return 0;
+        }
+
+        // Shifts range from -(n - 1) to (n - 1), offset by n to stay positive
+        int[][] count = new int[2 * n + 1][2 * n + 1];
+        int maxOverlap = 0;
+
+        for (int[] p1 : ones1) {
+            for (int[] p2 : ones2) {
+                int dr = p2[0] - p1[0] + n;
+                int dc = p2[1] - p1[1] + n;
+
+                count[dr][dc]++;
+                if (count[dr][dc] > maxOverlap) {
+                    maxOverlap = count[dr][dc];
+                }
+            }
+        }
+
+        return maxOverlap;
+    }
+}
